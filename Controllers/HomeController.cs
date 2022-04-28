8105e2d468d684dff088cede566357c706cdad03
abdoo431 <34003275+abdoo431@users.noreply.github.com>
2022-04-28 12:28:12 +0200

    add pro

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRCodeZxing.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+
+namespace QRCodeZxing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostEnvironment)
+        {
+            _logger = logger;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection formCollection)
+        {
+            var writer = new QRCodeWriter();
+            var resultBit = writer.encode(formCollection["QRCodeString"], BarcodeFormat.QR_CODE, 200, 200);
+            var matrix = resultBit;
+            int scale = 2;
+            Bitmap result = new Bitmap(matrix.Width * scale, matrix.Height * scale);
+            for (int x = 0; x < matrix.Height; x++)
+            {
+                for (int y = 0; y < matrix.Width; y++)
+                {
+                    Color pixel = matrix[x, y] ? Color.DarkSlateGray : Color.White;
+                    for (int i = 0; i < scale; i++)
+                        for (int j = 0; j < scale; j++)
+                        {
+                            result.SetPixel(x * scale + i, y * scale + j, pixel);
+                        }
+                }
+            }
+            string webRootPath = _hostEnvironment.WebRootPath;
+            string QrImageName = Guid.NewGuid().ToString();
+            result.Save(webRootPath + "\\images\\" + QrImageName + ".png") ;
+            ViewBag.Url = "\\images\\" + QrImageName + ".png";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
